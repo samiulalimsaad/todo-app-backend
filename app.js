3a99392d9098106eb4c042d34396653b6073c10a
samiulalimsaad <saadnbiuraj@gmail.com>
2022-05-19 00:09:13 +0600

    get method added

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import cors from "cors";
 import "dotenv/config";
 import express from "express";
+import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
+import { Todo } from "./todo.model.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,6 +13,25 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 
 app.use(express.json());
+
+app.get("/", async (req, res) => {
+    try {
+        const allTodo = await Todo.find({});
+        res.json({ allTodo, success: true });
+    } catch (error) {
+        res.json({ success: false, error: error.message });
+    }
+});
+
+app.get("/:id", async (req, res) => {
+    try {
+        const allTodo = await Todo.findById({ _id: req.params.id || "" });
+        res.json({ allTodo, success: true });
+    } catch (error) {
+        res.json({ success: false, error: error.message });
+    }
+});
+
 
 app.listen(PORT, async () => {
     console.log(`server is running at http://localhost:${PORT}`);
