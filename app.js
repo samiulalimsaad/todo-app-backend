b775485869c2459de587ab2bf8a69bf283f30ee3
samiulalimsaad <saadnbiuraj@gmail.com>
2022-05-19 00:08:49 +0600

    cors enabled

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-e;
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//  middleWares
+// enable cors
+app.use(cors());
+
+app.use(express.json());
+
+app.listen(PORT, async () => {
+    console.log(`server is running at http://localhost:${PORT}`);
+    mongoose.connect(
+        process.env.DATABASE_URL,
+        {
+            useNewUrlParser: true,
+        },
+        () => {
+            console.log("Database is connected");
+        }
+    );
+});
