166c2f85306ec61ebdf39c747a6e08acd1d0f659
samiulalimsaad <saadnbiuraj@gmail.com>
2022-05-19 00:13:26 +0600

    login method added

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 import cors from "cors";
 import "dotenv/config";
 import express from "express";
-import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
-import { Todo } from "./todo.model.js";
+import { Todo } from "./Models/todo.model.jsodel.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,8 +10,28 @@ const PORT = process.env.PORT || 5000;
 //  middleWares
 // enable cors
 app.use(cors());
-
 app.use(express.json());
+
+const verifyUser = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).json({
+            success: false,
+            message: "unauthorized access",
+        });
+    }
+    const token = authHeader.split(" ")[1];
+    await jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(403).json({
+                success: false,
+                message: "Forbidden access",
+            });
+        }
+        req.email = decoded?.email;
+        next();
+    });
+};
 
 app.get("/", async (req, res) => {
     try {
@@ -61,6 +80,18 @@ app.delete("/:id", async (req, res) => {
         res.json({ success: false, error: error.message });
     }
 });
+
+app.post("/login", async (req, res) => {
+    const token = jwt.sign(req.body, process.env.ACCESS_TOKEN, {
+        expiresIn: "1d",
+    });
+    res.json({
+        token,
+        success: true,
+    });
+});
+
+
 
 app.listen(PORT, async () => {
     console.log(`server is running at http://localhost:${PORT}`);
